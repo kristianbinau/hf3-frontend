5afef13fed74feb0560d9fd6e80c376a197e655e
Kristian Binau <kristian@binau.me>
2021-08-30 13:28:30 +0200

    Test

---
+++ pages/fun.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>This is the fun page</p>
+    <NuxtLink to="/notfun">Notfun page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/notfun.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <p>This is the not fun page</p>
+    <NuxtLink to="/fun">Fun page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
